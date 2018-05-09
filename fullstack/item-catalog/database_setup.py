de239d0a47b33d23bef31d96c950694200b11289
ntromboukis <ntromboukis@gmail.com>
2018-05-09 15:33:04 -0400

    chore: pep8 cleaning

--- fullstack/item-catalog/database_setup.py
+++ fullstack/item-catalog/database_setup.py
@@ -5,14 +5,16 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 # sets up a user table
 class User(Base):
     __tablename__ = 'user'
 
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     picture = Column(String(80))
     email = Column(String(250), nullable=False)
+
 
 # sets up a store table
 class Store(Base):
@@ -27,39 +29,39 @@ class Store(Base):
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-           'photo'        : self.photo,
-           'category'     : self.category
-       }
+        """Return object data in easily serializeable format"""
+        return {
+            'name':       self.name,
+            'id':         self.id,
+            'photo':      self.photo,
+            'category':   self.category
+        }
+
 
 # sets up a catalog_item table
 class CatalogItem(Base):
     __tablename__ = 'catalog_item'
 
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     price = Column(String(8))
-    store_id = Column(Integer,ForeignKey('store.id'))
+    store_id = Column(Integer, ForeignKey('store.id'))
     store = relationship(Store)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'  : self.description,
-           'id'           : self.id,
-           'price'        : self.price,
-       }
+        """Return object data in easily serializeable format"""
+        return {
+           'name':          self.name,
+           'description':   self.description,
+           'id':            self.id,
+           'price':         self.price,
+        }
 
-
-
+        
 engine = create_engine('sqlite:///itemcatalog.db')
 
 
